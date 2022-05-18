da2ba9fbd972a224436d34b696aebf9626cbc67b
Nanai Jua <me@n7i.dev>
2022-05-18 23:50:49 +0900

    🚨 fix using `{}` .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "next-themes";
 
 import "styles/globals.css";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
   applyLayout?: (page: JSX.Element) => JSX.Element;
 };
 
